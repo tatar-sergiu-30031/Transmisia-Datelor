da880b06f1308f5bd5e08f3e23c01f2f77e533a8
tatar-sergiu-30021 <tatarsergiu@yahoo.com>
2021-03-06 15:30:57 +0200

    Laborator1

--- lab-1/2/app.js
+++ lab-1/2/app.js
@@ -1,24 +1,26 @@
 var counter = 0;
 
-function printValue ( divId , value ){
-    document . getElementById ( divId ) . innerHTML = value ;
-}
-printValue ('counter', 0);
+   function printValue ( divId , value ){
+       document . getElementById ( divId ) . innerHTML = value ;
+   }
+   printValue ('counter', 0);
 
-document . getElementById ('inc') . addEventListener ('click', increment );
+   document . getElementById ('inc') . addEventListener ('click', increment );
 
-function increment () {
-    if(counter>=0 && counter<10){
-        counter++;
-        printValue ('counter', counter );
-    }
-}
+   function increment () {
+       if(counter>=0 && counter<10){
+           counter++;
+           printValue ('counter', counter );
+       }
+   }
 
-document . getElementById ('dec') . addEventListener ('click', decrement );
+   document . getElementById ('dec') . addEventListener ('click', decrement );
 
 
-function decrement () {
-    if(counter>0 && counter<=10) {
-        counter--;
-        printValue('counter', counter);
-    }
+   function decrement () {
+       if(counter>0 && counter<=10) {
+           counter--;
+           printValue('counter', counter);
+       }
+   }
+
--- lab-1/2/appTest.js
+++ lab-1/2/appTest.js
@@ -1,3 +1,4 @@
+
 function test() {
   console.log(sum(0) == 0 ? ' Passed ' : ' Failed ');
   console.log(sum(2) == 3 ? ' Passed ' : ' Failed ');
@@ -5,3 +6,23 @@ function test() {
   console.log(sum() == 'n is undefined ' ? ' Passed ' : ' Failed ');
 }
 test();
+
+
+
+index.html
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel= 'stylesheet' href= 'style.css '>
+  <meta charset="UTF-8">
+  <title>Second js app</title>
+</head>
+<body>
+<div id='counter'></div>
+<button id='inc'>+</button>
+<button id='dec'>-</button>
+<script src='app.js'></script>
+</body>
+</html>
